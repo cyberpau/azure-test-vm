d28e6cd0000313e04382945cdc1d5518dfc2f039
cyberpau <johnpaulomataac@gmail.com>
2021-11-29 18:17:40 +0800

    add readme to create azure rg and spring-cloud instance

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# azure-test-vm
+# Azure Test Environment
+
+## Prerequisite
+
+
+Before doing `vagrant up`, you can uncomment `config.vm.provision "shell", path: "bootstrap.sh"` from `.\bootstrap.sh` to automatically install DNF and Azure CLI. If you want to install it manually one by one, please follow the steps below:
+
+
+### Install DNF
+
+```
+sudo yum install epel-release -y
+sudo yum install dnf -y
+```
+
+
+### Install Azure CLI
+
+https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=dnf
+
+Then login your azure account:
+
+```
+az login # Sign into an azure account
+az account show # See the currently signed-in account.
+```
+
+<BR>
+
+## Create an Azure Spring Cloud instance
+
+1. Set the global variables:
+
+    ```
+    RESOURCE_GROUP_NAME=jmataac-springtest-rg2
+    SPRING_CLOUD_NAME=jmataac-springtest-s1
+    ```
+
+2. Create resource group:
+    ```
+    az group create \
+        -g "$RESOURCE_GROUP_NAME" \
+        -l eastus
+    ```
+
+3. And then create spring cloud instance:
+    ```
+    az spring-cloud create \
+        -g "$RESOURCE_GROUP_NAME" \
+        -n "$SPRING_CLOUD_NAME" \
+        --sku standard \
+        --enable-java-agent
+    ```
+
+3. Set resource group and spring cloud instance by default:
+
+    ```
+    az configure --defaults group=${RESOURCE_GROUP_NAME}
+    az configure --defaults spring-cloud=${SPRING_CLOUD_NAME}
+    ```
--- bootstrap.sh
+++ bootstrap.sh
@@ -6,72 +6,16 @@ if [[ $EUID -gt 0 ]]
   exit
 fi
 
-if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
-  echo "Starting bootstrap.sh ..."
-else
-  echo "Unable to connect to internet"
-  exit 
-fi
+## Install DNF
+sudo yum install epel-release -y
+sudo yum install dnf -y
 
-## Update and install pre-requisites
-sudo yum update -y
-
-## Install Docker Latest Version
-sudo yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker "vagrant"
-
-if cd 0; ## RUN ONLY ONCE!!!!
-  then echo "K8 configuration already completed"; 
-  else
-
-## Disable and turn off SWAP
-swapoff -a; sed -i '/swap/d' /etc/fstab
-
-
-## Stop and Disable firewall
-systemctl disable firewalld; systemctl stop firewalld
-
-## Disable SELinux
-setenforce 0
-sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
-
-## Update sysctl settings for Kubernetes networking
-cat >>/etc/sysctl.d/kubernetes.conf<<EOF
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sysctl --system
-
-## Add Kubernetes Yum Repo
-cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+## Install Azure CLI
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[azure-cli]
+name=Azure CLI
+baseurl=https://packages.microsoft.com/yumrepos/azure-cli
 enabled=1
 gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-exclude=kubelet kubeadm kubectl
-EOF
-
-## Install Kubernetes Components
-sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
-
-## Update /etc/hosts file
-cat >>/etc/hosts<<EOF
-192.168.50.100   kmaster-1.cyberpau.com    kmaster-1
-192.168.50.101   kworker-1.cyberpau.com    kworker-1
-192.168.50.102   kworker-2.cyberpau.com    kworker-2
-EOF
-
-## Enable and start kubelet service
-systemctl enable --now kubelet
-
-## Finally,
-mkdir 0
-fi
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo
+sudo dnf install azure-cli
